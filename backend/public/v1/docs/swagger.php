8bace946c4b6d6914a12f88e60d2583885133560
FikretZajmovic <fikret.zajmovic@stu.ibu.edu.ba>
2024-06-03 18:49:12 +0200

    swagger fix

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -7,6 +7,6 @@ define('BASE_URL', 'https://goldfish-app-l87ee.ondigitalocean.app/backend/');
 error_reporting(0);
 
 $openapi = \OpenApi\Generator::scan(['../../../rest', './'], ['pattern' => '*.php']);
-header('Content-Type: application/x-yaml');
-echo $openapi->toYaml();
+header('Content-Type: application/json');
+echo $openapi->toJson();
 ?>
