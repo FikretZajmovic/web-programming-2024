5e7271b1dfed4aa06847909ef33c77e2a980085d
FikretZajmovic <fikret.zajmovic@stu.ibu.edu.ba>
2024-06-03 18:55:35 +0200

    change

--- backend/index.php
+++ backend/index.php
@@ -1,21 +1,10 @@
 <?php
 
 require 'vendor/autoload.php';
-// require 'rest/routes/middleware_routes.php';
+require 'rest/routes/middleware_routes.php';
 require 'rest/routes/product_routes.php';
 require 'rest/routes/user_routes.php';
 require 'rest/routes/review_routes.php';
 require 'rest/routes/auth_routes.php';
-
-function allow_preflight() {
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        header('Access-Control-Allow-Headers: Request, Origin, Content-Type');
-        header('Access-Control-Allow-Origin: *');
-        die();
-    } else {
-        header('Access-Control-Allow-Origin: *');
-    }
-}
-allow_preflight();
 
 Flight::start();
