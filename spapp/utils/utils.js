8aa019b8160c80273c663e78d48aa42ee6a724ed
FikretZajmovic <fikret.zajmovic@stu.ibu.edu.ba>
2024-06-04 18:08:58 +0200

    Fix

--- spapp/utils/utils.js
+++ spapp/utils/utils.js
@@ -1,9 +1,9 @@
 const Utils = {
   get_from_localstorage: function (key) {
-    return JSON.parse(window.localStorage.getItem(key));
+    return window.localStorage.getItem(key);
   },
   set_to_localstorage: function (key, value) {
-    window.localStorage.setItem(key, value);
+    window.localStorage.setItem(key, JSON.stringify(value));
   },
   logout: function () {
     localStorage.clear();
